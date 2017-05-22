d5890c1d0a2e3bb325ad37cd971aa1d08b5d4997
yongjie_zou <zouyongjie@haixue.com>
2017-05-22 17:53:38 +0800

    finish first game

---
+++ SpriteKitSimpleGame/SpriteKitSimpleGame/GameOverScene.swift
@@ -0,0 +1,39 @@
+//
+//  GameOverScene.swift
+//  SpriteKitSimpleGame
+//
+//  Created by yongjie_zou on 2017/5/22.
+//  Copyright © 2017年 yongjie_zou. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class GameOverScene: SKScene {
+    init(size: CGSize, won:Bool) {
+        super.init(size: size)
+        
+        backgroundColor = SKColor.white
+        let message = won ? "You Won!" : "You Lose :["
+        
+        let label = SKLabelNode(fontNamed: "Chalkduster")
+        label.text = message
+        label.fontSize = 40
+        label.fontColor = SKColor.black
+        label.position = CGPoint.init(x: size.width / 2, y: size.height / 2)
+        addChild(label)
+        
+        run(SKAction.sequence([
+            SKAction.wait(forDuration: 3),
+            SKAction.run() {
+                let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+                let scene = GameScene.init(size: size)
+                self.view?.presentScene(scene, transition: reveal)
+            }
+            ]))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
